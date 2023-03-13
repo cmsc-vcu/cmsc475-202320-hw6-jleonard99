bc2874102680d10ac522320d8c44f2e09025dffe
John Leonard <john@lowkeylabs.com>
2023-03-12 20:45:24 -0400

    correct placement of state

--- hw6/App.js
+++ hw6/App.js
@@ -1,12 +1,12 @@
 
-import React, {createContext, useState, useCallback} from 'react';
+import React, {createContext, useState, useCallback, useEffect} from 'react';
 
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { globalStyles } from './pages/GlobalStyles.js';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { MyScreen } from "./pages/MyScreen.js";
+import { MyScreen,MyButton } from "./pages/MyScreen.js";
 import { MyScreen2 } from "./pages/MyScreen2.js";
 import { database } from "./pages/MyDatabase";
 
@@ -22,30 +22,43 @@ function App() {
 
 
 export default function MyTabs() {
-  const [ pressCount, setPressCount ] = useState( 0 );
+  const [ pressCount , setPressCount ] = useState( 0 );
+  const [ imageId, setImageId ] = useState( 0 );
 
 //  const MyDummyFunc = () => {
 //    console.log("my dummy func",pressCount );
 //    setPressCount( 10 );
 //  }
 
-  const MyDummyFunc = useCallback( val => {setPressCount(val);},[]);
+  function incrementCounter ( value ) {
+    console.log( "value: ",value," pressCount:",pressCount );
+    setPressCount( pressCount + 1 );
+  }
+
+  useEffect( ()=>{
+    setImageId( Math.floor(Math.random() * database.length ) );
+
+  },[pressCount]);
   
   function MyScreenWrapper(){
     return (
       <>
       <Text>{pressCount}</Text>
-      <MyScreen database={database} buttonCount={ 4 } onGuess={ MyDummyFunc } />
+      <MyScreen database={database} pressCount={ pressCount } initImage={ imageId } onGuess={ incrementCounter } />
       </>
     )
   }
   
   return (
+    <>
+    <MyButton onPressed={incrementCounter} key={10} name={"Button"} id={10} />
     <NavigationContainer>
         <Tab.Navigator>
-          <Tab.Screen name="MyScreen" component={ MyScreenWrapper } />
+          <Tab.Screen name="MyScreen" component={ MyScreenWrapper }  />
           <Tab.Screen name="MyScreen2" component={ MyScreen2 } />
         </Tab.Navigator>
     </NavigationContainer>
+    </>
   );
 }
+
